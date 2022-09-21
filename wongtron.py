5ad6319cbae5f6f969ddc2d3c67672c2e14af30f
Nestor <nlzx432@gmail.com>
2022-09-21 18:34:51 -0400

    init board

--- wongtron.py
+++ wongtron.py
@@ -16,10 +16,24 @@ NAME = 'wongtron';
 TURN_INDICATOR_FILENAME = (NAME + '.go');
 
 # wongtron state enum
-class State(Enum):
-    WAITING_FOR_OPPONENT_TURN = 0; # after our turn, waiting for wongtron.go deletion
+class WongtronState(Enum):
+    WAITING_FOR_OPP_TURN = 0; # after our turn, waiting for wongtron.go deletion
     WAITING_FOR_TURN = 1; # waiting to see wongtron.go
     PLAYING = 2;
+
+# board cell state enum
+class CellState(Enum):
+    EMPTY = 0;
+    WONG = 1;
+    OPP = 2;
+
+def init_board():
+    board = [];
+    for row in range(9):
+        board.append([]);
+        for cell in range(9):
+            board[row].append(CellState.EMPTY);
+    return board;
 
 def file_present(filename):
     cwd = os.getcwd();
@@ -31,27 +45,29 @@ def play():
     pass;
 
 def main():
-    state = State.WAITING_FOR_TURN;
+    # init game variables
+    state = WongtronState.WAITING_FOR_TURN;
+    board = init_board();
     while(True):
         
         # wait for referee to remove the wongtron.go file
-        if state == State.WAITING_FOR_OPPONENT_TURN:
+        if state == WongtronState.WAITING_FOR_OPP_TURN:
             if not file_present(TURN_INDICATOR_FILENAME):
-                state = State.WAITING_FOR_TURN;
+                state = WongtronState.WAITING_FOR_TURN;
             else:
                 sleep(WAIT_REFRESH_SECONDS);
 
         # wait for wongtron.go file
-        elif state == State.WAITING_FOR_TURN:
+        elif state == WongtronState.WAITING_FOR_TURN:
             if file_present(TURN_INDICATOR_FILENAME):
-                state = State.PLAYING;
+                state = WongtronState.PLAYING;
             else:
                 sleep(WAIT_REFRESH_SECONDS);
 
         # play our turn 
-        elif state == State.PLAYING:
+        elif state == WongtronState.PLAYING:
             play();
-            state = State.WAITING_FOR_OPPONENT_TURN;
+            state = WongtronState.WAITING_FOR_OPP_TURN;
 
 if __name__ == '__main__':
     main();
