c9163d70ce2f8f22c3a60093100124aec59ad267
Nestor <nlzx432@gmail.com>
2022-09-21 22:17:15 -0400

    parse_pregame_moves()

--- wongtron.py
+++ wongtron.py
@@ -1,21 +1,38 @@
+# -------------------------------------- #
 #  WONGTRON
 #  CS4341
 #  Aria Yan
 #  Jacob Salerno
 #  Nestor Lopez
+# -------------------------------------- #
+
+# -------------------------------------- #
+# wongtron config variables
+# -------------------------------------- #
+
+PREGAME_MOVES_FILENAME = 'first_four_moves';
+WAIT_REFRESH_SECONDS = 1;
+NAME = 'wongtron';
+
+# -------------------------------------- #
+# imports
+# -------------------------------------- #
 
 from time import sleep
 from enum import Enum
 import os
 
-# wongtron config variables
-WAIT_REFRESH_SECONDS = 1;
-NAME = 'wongtron';
-
+# -------------------------------------- #
 # derived variables
+# -------------------------------------- #
+
 TURN_INDICATOR_FILENAME = (NAME + '.go');
 
-# wongtron state enum
+# -------------------------------------- #
+# enums
+# -------------------------------------- #
+
+# wongtron state
 class WongtronState(Enum):
     WAITING_FOR_OPP_TURN = 0; # after our turn, waiting for wongtron.go deletion
     WAITING_FOR_TURN = 1; # waiting to see wongtron.go
@@ -26,13 +43,20 @@ class CellState(Enum):
     EMPTY = 0;
     WONG = 1;
     OPP = 2;
+ 
+# -------------------------------------- #
+# board types
+# -------------------------------------- #
 
-# move class
 class Move():
     def __init__(self, wongtron, board, cell):
         self.wongtron = wongtron;
         self.board_number = board;
         self.cell_number = cell;
+
+# -------------------------------------- #
+# board functions
+# -------------------------------------- #
 
 # initialize a classic board. (1x9 array of CellState's, index 4 is center cell)
 def init_classic_board():
@@ -55,6 +79,10 @@ def apply_move(boards, move):
    cell_state = CellState.WONG if move.wongtron else CellState.OPP;
    boards[board_number][cell_number] = cell_state;
 
+# -------------------------------------- #
+# file IO functions
+# -------------------------------------- #
+
 # check if the given filename is present in cwd
 def file_present(filename):
     cwd = os.getcwd();
@@ -62,8 +90,38 @@ def file_present(filename):
     file_present = filename in directory_contents;
     return file_present;
 
+def parse_pregame_moves():
+    move_lines = [];
+    moves = [];
+
+    # the first and third pregame moves belong to whoever gets the first turn.
+    wongtrons_turn_first = file_present(TURN_INDICATOR_FILENAME);
+
+    # parse first four moves
+    with open(PREGAME_MOVES_FILENAME) as f:
+        move_lines = f.readlines();
+    for i, line in enumerate(move_lines):
+        split_move_line = line.split();
+        board_num = int(split_move_line[1]);
+        cell_num = int(split_move_line[2]);
+
+        # this move begins to wong if wong is first turn and this is the first or third pregame move
+        wongs_move = wongtrons_turn_first if (i % 2 == 0) else not wongtrons_turn_first;
+        
+        moves.append(Move(wongs_move, board_num, cell_num));
+
+    return moves;
+
+# -------------------------------------- #
+# play functions
+# -------------------------------------- #
+
 def play():
     pass;
+
+# -------------------------------------- #
+# main
+# -------------------------------------- #
 
 def main():
     # init game variables
