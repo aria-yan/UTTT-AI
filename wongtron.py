05c596bc8b526589e1f1b2bf79fcd33139d982c0
aria-yan <aria.yan.d@gmail.com>
2022-09-26 16:28:19 -0400

    added writing to move file

--- wongtron.py
+++ wongtron.py
@@ -129,6 +129,11 @@ def parse_move_file():
 
     return moves;
 
+#append given move to move file
+def write_move_file(move):
+    move_file = open("move_file", "a")
+    move_file.write("\n"+str(move.wongtron)+" "+str(move.board_number)+" "+str(move.cell_number))
+
 # -------------------------------------- #
 # play functions
 # -------------------------------------- #
